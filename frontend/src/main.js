1d20e2f75d135e21df914e5afe3a8abf8a5cf438
sinological <sinological@gmail.com>
2025-12-18 20:00:51 +0800

    chore: add build version log

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+console.log('BUILD VERSION:', new Date().toISOString());
+
 createApp(App).use(router).mount('#app');
